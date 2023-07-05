efcf8c65d9eb907c5b68608e7986c7e0a79bb97a
alexperez <alexperez@salesforce.com>
2023-07-05 17:15:02 -0300

    chore: fix test

--- test/auth-methods/oauth2-client-credentials-method.test.js
+++ test/auth-methods/oauth2-client-credentials-method.test.js
@@ -165,7 +165,7 @@ describe('OAuth 2, client credentials method', () => {
       const button = /** @type HTMLElement */ (element.shadowRoot.querySelector('.adv-settings-input'));
       button.click();
       await nextFrame();
-      assert.equal(getComputedStyle(section).display, 'block', 'section is not hidden');
+      assert.equal(getComputedStyle(section).display, 'inline-grid', 'section is not hidden');
     });
 
     it('does not render PKCE checkbox', async () => {
